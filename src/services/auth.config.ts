f98c7f5addd19a042e844807337c10dde299963e
Getúlio Vinicius <getuliovinits@gmail.com>
2018-04-27 19:39:03 -0300

    teste

--- src/services/auth.config.ts
+++ src/services/auth.config.ts
@@ -6,5 +6,5 @@ export const AUTH_CONFIG = {
   // Needed for Auth0Cordova (capitalization: Id):
   clientId: '{5voHLYcwogSEm2NHpGmbNdQ2y0anLk4f}',
   domain: '{getuliovinicius.auth0.com}', // e.g., you.auth0.com
-  packageIdentifier: 'br.gov.sp.fatec.franca.getulio.loja' // config.xml widget ID
+  packageIdentifier: 'com.auth0.ionic' // config.xml widget ID
 };
