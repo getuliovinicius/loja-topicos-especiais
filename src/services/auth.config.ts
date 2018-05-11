deb10fed69b97941087219faaf9ddbfba0454d6f
Getúlio Vinicius <getuliovinits@gmail.com>
2018-05-10 21:54:12 -0300

    teste

--- src/services/auth.config.ts
+++ src/services/auth.config.ts
@@ -2,9 +2,9 @@
 
 export const AUTH_CONFIG = {
   // Needed for Auth0 (capitalization: ID):
-  clientID: '{5voHLYcwogSEm2NHpGmbNdQ2y0anLk4f}',
+  clientID: '5voHLYcwogSEm2NHpGmbNdQ2y0anLk4f',
   // Needed for Auth0Cordova (capitalization: Id):
-  clientId: '{5voHLYcwogSEm2NHpGmbNdQ2y0anLk4f}',
+  clientId: '5voHLYcwogSEm2NHpGmbNdQ2y0anLk4f',
   domain: 'getuliovinicius.auth0.com', // e.g., you.auth0.com
   packageIdentifier: 'com.auth0.ionic' // config.xml widget ID
 };
